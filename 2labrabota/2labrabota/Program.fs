4a48490143cc0a837d0050396731e0c830fb8f51
kukorra <154669365+kukorra@users.noreply.github.com>
2025-02-27 18:41:05 +0500

    Add files via upload

---
+++ 2labrabota/2labrabota/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+// Функция для подсчёта количества цифр в числе
+let countchisel (n: int) =
+    let mutable num = n
+    let mutable count = 0
+    if num = 0 then  // Учитываем случай, когда число равно 0
+        count <- 1
+    else
+        while num > 0 do
+            num <- num / 10
+            count <- count + 1
+    count
+
+// Чтение числа с клавиатуры
+printfn "Введите натуральное число:"
+let vvod = Console.ReadLine()
+
+// Преобразуем строку в число с обработкой исключений
+try
+    let chislo = int vvod  // Пытаемся преобразовать строку в число
+    let Countchisel = countchisel chislo
+    printfn "Количество цифр в числе: %d" Countchisel
+with
+| :? System.FormatException -> printfn "Ошибка: введено не число."
