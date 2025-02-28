bb90ff6179f9fd5add47de7df85d4eb969c54438
kukorra <154669365+kukorra@users.noreply.github.com>
2025-02-28 09:52:48 +0500

    Add files via upload

---
+++ 2labrabota/2labrabota/Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+
+let rec countDigits (n : int) =
+    if n < 0 then
+        failwith "Число должно быть натуральным (положительным)."
+    else if n < 10 then
+        1
+    else
+        1 + countDigits (n / 10)
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите натуральное число:"
+    let input = Console.ReadLine()
+
+    try
+        let number = Int32.Parse input
+        if number < 0 then
+            printfn "Пожалуйста, введите натуральное (положительное) число."
+        else
+            let digitCount = countDigits number
+            printfn "Количество цифр в числе %d: %d" number digitCount
+    with
+    | :? FormatException ->
+        printfn "Ошибка: Введен некорректный формат числа."
+    | ex ->
+        printfn "Произошла ошибка: %s" ex.Message
+
+    Console.ReadKey() |> ignore
+    0
+
